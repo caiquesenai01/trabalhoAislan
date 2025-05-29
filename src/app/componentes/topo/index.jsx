f77c4b4e348cb1fb601e6ee1d06dace3ca2cdb11
MahJunior <marcos.a.junior7@ba.estudante.senai.br>
2025-05-29 02:51:46 -0300

    Add files via upload

--- src/app/componentes/topo/index.jsx
+++ src/app/componentes/topo/index.jsx
@@ -1,6 +1,5 @@
 import styles from "./topo.module.css";
 import Image from "next/image";
-import restaurante from "@/../../public/banner.png";
 
 export default function Topo(){
     return(
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Topo from "./componentes/topo";
-import bru from "/public/bruschetta.jpg";
+import bru from "../../public/bruschetta.jpg";
 import Cardapio  from "./componentes/cardapio";
 import entradas from "../../public/entrada.png";
 import massas from "../../public/massa.png";
-import lupa from "../../public/lupa.png"
+import lupa from "../../public/lupa.png";
+import carne from "../../public/carne.png";
+import bebida from "../../public/bebidas.png";
+import salada from "../../public/salada.png";
+import sobre from "../../public/sobremesa.png";
+
+
+
+
 
 export default function Home() {
   return (
@@ -16,11 +24,11 @@ export default function Home() {
 
       <div className={styles.botao}>
         <button><Image width={20} height={20} src={entradas}/>Entradas</button>
-        <button><Image width={20} height={20} src={entradas}/>Massas</button>
-        <button><Image width={20} height={20} src={entradas}/>Carnes</button>
-        <button><Image width={20} height={20} src={entradas}/>Bebidas</button>
-        <button><Image width={20} height={20} src={entradas}/>Saladas</button>
-        <button><Image width={20} height={20} src={entradas}/>Sobremesas</button>
+        <button><Image width={20} height={20} src={massas}/>Massas</button>
+        <button><Image width={20} height={20} src={carne}/>Carnes</button>
+        <button><Image width={20} height={20} src={bebida}/>Bebidas</button>
+        <button><Image width={20} height={20} src={salada}/>Saladas</button>
+        <button><Image width={20} height={17} src={sobre}/>Sobremesas</button>
       </div>
 
       <div className={styles.pesquisa}>
@@ -47,6 +55,18 @@ export default function Home() {
               <Cardapio imagem ={bru} nome ={"Bruschetta"} tipo ={"Entrada"} preco ={"R$ 9,20"}
               info={"Prato original da itália, que leva fatias de pão italiano torrado, tomate picado, alho, manjericão e azeite."}          
               />
+              </div>
+              <div>
+              <div className={styles.baixo}>
+                <Cardapio imagem ={bru} nome ={"Bruschetta"} tipo ={"Entrada"} preco ={"R$ 9,20"}
+                info={"Prato original da itália, que leva fatias de pão italiano torrado, tomate picado, alho, manjericão e azeite."}          
+                />
+              </div>
+              <div>
+                <Cardapio imagem ={bru} nome ={"Bruschetta"} tipo ={"Entrada"} preco ={"R$ 9,20"}
+                info={"Prato original da itália, que leva fatias de pão italiano torrado, tomate picado, alho, manjericão e azeite."}          
+                />
+              </div>
               </div>
             </div>
           </div>
